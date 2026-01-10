649fa945eb65178d6a549cfa9da4c8e48cd86fdd
toyota-m2k <toyota.mitsuki@gmail.com>
2026-01-10 09:41:51 +0900

    feat: material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
