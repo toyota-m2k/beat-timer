b9de43a4429ff1fda5c4ad09315b605572bd76b5
toyota-m2k <toyota.mitsuki@gmail.com>
2026-01-10 09:04:53 +0900

    feat: edge-to-edge

--- app/src/main/java/com/michael/beattimer/MainActivity.kt
+++ app/src/main/java/com/michael/beattimer/MainActivity.kt
@@ -7,7 +7,10 @@ import android.view.View
 import android.view.WindowManager
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.michael.beattimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +21,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         controls = ActivityMainBinding.inflate(layoutInflater)
         setContentView(controls.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(controls.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         ticker.observables.loopCounter.observe(this) {
             updateCounter()
